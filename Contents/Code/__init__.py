11ab02cf20e05a13625c234e7911196680bd9f3b
fansforyouauthor <fansforyouauthor>
2022-02-27 07:45:08 -0600

    Fix year determination

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -3,7 +3,7 @@ import datetime
 import json
 
 media_name_regex = r"(Only[F|f]ans[ ]?)\-? (.+)( \-)? ([0-9]{4})[ ]?\-?([0-9]{2})[ ]?\-?([0-9]{2})[ ]?\-? ([0-9]+)[ ]?\-? (.+)"
-result_id_regex = r"onlyfans::actor::(.+)::scene_id::([0-9]+)"
+result_id_regex = r"onlyfans::actor::(.+)::scene_id::([0-9]+)::post_year::([0-9]+)::post_month::([0-9]+)::post_day::([0-9]+)"
 
 actor_portrait_urls = json.loads(Prefs["actor_portrait_urls"])
 default_actor_portrait_url = Prefs["default_actor_portrait_url"]
@@ -29,10 +29,12 @@ class FansForYouAgent(Agent.Movies):
 
     artist = regex_match.group(2)
     year = regex_match.group(4)
+    month = regex_match.group(5)
+    day = regex_match.group(6)
     scene_id = regex_match.group(7)
     scene_title = regex_match.group(8)
 
-    results.Append(MetadataSearchResult(id="onlyfans::actor::" + artist + "::scene_id::" + scene_id, name=scene_title, year=int(year), lang='en', score=100))
+    results.Append(MetadataSearchResult(id="onlyfans::actor::" + artist + "::scene_id::" + scene_id + "::post_year::" + year + "::post_month::" + month + "::post_day::" + day, name=scene_title, year=int(year), lang='en', score=100))
 
   def update(self, metadata, media, lang):
     regex_match = re.search(result_id_regex, metadata.id)
@@ -42,6 +44,9 @@ class FansForYouAgent(Agent.Movies):
 
     artist = regex_match.group(1)
     scene_id = regex_match.group(2)
+    year = int(regex_match.group(3))
+    month = int(regex_match.group(4))
+    day = int(regex_match.group(5))
 
     # Actors
     metadata.roles.clear()
